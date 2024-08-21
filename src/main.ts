8ad9fc1c248e8a4343eeff7206aa965e4b3e5bd1
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2024-08-21 11:41:13 -0500

    Se agregan a los dtos

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -10,6 +10,20 @@ async function bootstrap() {
       forbidNonWhitelisted: true
     }
   ));
+    // Activar CORS para cualquier origen
+    app.enableCors({
+      origin: '*', // Permitir todos los orígenes
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+      credentials: false, // No se permiten credenciales (cookies, autenticaciones HTTP, etc.)
+    });
+    const config=new DocumentBuilder()
+    .setTitle('API documentation')
+    .setDescription('API :D')
+    .setVersion('0.01')
+    .addTag('Sushi')
+    .build();
+    const document=SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('api',app,document);
   await app.listen(3000);
 }
 bootstrap();
--- src/owners/dto/create-owner.dto.ts
+++ src/owners/dto/create-owner.dto.ts
@@ -2,8 +2,11 @@ import { Type } from "class-transformer";
 import { IsArray, IsOptional, IsString, MinLength, ValidateNested } from "class-validator";
 import { Car } from "src/cars/entities/car.entity";
 import { CreateCarRefDto } from "./create-ref-dto";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOwnerDto {
+
+    @ApiProperty({ example: 'Juan Camilo', description: 'The name of the person' })
     @IsString()
     @MinLength(2)
     name:string;
